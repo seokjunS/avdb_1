fc56c6b1faf7c9e2decad0a1c4a8c1ba58c3f006
seokjunseo <dane2522@snu.ac.kr>
2016-03-14 19:45:09 +0900

    add jars

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("sentence");
+			TokenizerDemo.run(args);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+
+	}
+
+}
